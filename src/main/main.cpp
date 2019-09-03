49e073fb722092355e22dfb87b9af23722e3c7a4
Kiran <dkiran@altiostar.com>
2019-09-03 15:59:46 +0530

    cmake changes

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include "common.hpp"
 using namespace std;
 int main(void) {
-       cout << "For Testing CMake" << endl;
+cout << "CMake:> Add Sub Mul Div\t" << add(20,30) << "\t" << sub(30,20)<<"\t" << mul(20,30) <<"\t" << divide(30,20) << endl;
        return(0);
 }
