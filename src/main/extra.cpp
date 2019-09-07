b6c1259f4905fe1657f7c17a5aeed594188200b7
kiran kumar D <kiranvenu80@gmail.com>
2019-09-07 19:51:40 +0530

    Adding other files

--- src/main/extra.cpp
+++ src/main/extra.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include "common.hpp"
-using namespace std;
-int main(int argc, char **argv) {
-	cout << "CMake:> Add Sub Mul Div\t" << add(20,30) << "\t" << sub(30,20)<<"\t" << mul(20,30) <<"\t" << divide(30,20) << endl;
-	return(0);
+
+void extrafunction()
+{
+    std::cout << "Hello World!\n";
 }
